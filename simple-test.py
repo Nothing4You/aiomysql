17ac2894fb7c1538f7d36ac220b55c460710bb2b
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2022-01-31 22:59:33 +0100

    testing keepalive

--- simple-test.py
+++ simple-test.py
@@ -1,10 +1,22 @@
 import asyncio
 import datetime
 import logging
+import socket
+
+
+def set_keep_alive(writer):
+    transport = writer.transport
+    transport.pause_reading()
+    raw_sock = transport.get_extra_info('socket', default=None)
+    raw_sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    transport.resume_reading()
 
 
 async def test_async():
     reader, writer = await asyncio.open_connection("127.0.0.1", 3306)
+
+    set_keep_alive(writer)
+
     b = await reader.readexactly(1)
     print(f"read 1 byte: {b=}")
     writer.write(bytes(0))
